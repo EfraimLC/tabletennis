e7ac002cea11ae35809d15eee1bac55302f9000c
EfraimLC <efraimlc02@gmail.com>
2026-06-22 22:11:41 -0300

    Add files via upload

---
+++ Produtos/produto.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('cardContainer');
+
+const cardLista[
+    { id: 1, titulo: "Florestas Tropicais", subtitulo: "O pulmão verde do nosso planeta.", imagem: "https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?w=500" },
+    { id: 2, titulo: "Montanhas Rochosas", subtitulo: "A imponência dos picos cobertos de neve.", imagem: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?w=500" },
+    { id: 3, titulo: "Oceanos Profundos", subtitulo: "Os mistérios escondidos nas águas azuis.", imagem: "https://images.unsplash.com/photo-1551244072-5d12893278ab?w=500" },
+    { id: 4, titulo: "Montanha", subtitulo: "Montanhas altas e baixas de diversos tamanhos e locais distintos.", imagem: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+];
