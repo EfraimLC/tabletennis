2cef373d327a3462a7aae5d8562f3ec27150d8e9
EfraimLC <efraimlc02@gmail.com>
2026-06-25 20:52:10 -0300

    Add files via upload

--- Produtos/produto.js
+++ Produtos/produto.js
@@ -1,8 +1,43 @@
-const container = document.getElementById('cardContainer');
+const container = document.getElementById("cardContainer");
 
-const cardLista[
-    { id: 1, titulo: "Florestas Tropicais", subtitulo: "O pulmão verde do nosso planeta.", imagem: "https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?w=500" },
-    { id: 2, titulo: "Montanhas Rochosas", subtitulo: "A imponência dos picos cobertos de neve.", imagem: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?w=500" },
-    { id: 3, titulo: "Oceanos Profundos", subtitulo: "Os mistérios escondidos nas águas azuis.", imagem: "https://images.unsplash.com/photo-1551244072-5d12893278ab?w=500" },
-    { id: 4, titulo: "Montanha", subtitulo: "Montanhas altas e baixas de diversos tamanhos e locais distintos.", imagem: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+const cardLista = [
+  {
+    id: 1,
+    titulo: "2 Raquetes Butterfly + 3 Bolinhas Butterfly",
+    subtitulo: "R$ 625,00",
+    imagem: "figure_produto1",
+  },
+  {
+    id: 2,
+    titulo: "1 Raquete Joola + 12 Bolinhas Joola",
+    subtitulo: "R$850,00",
+    imagem: "figure_produto2",
+  },
+  {
+    id: 3,
+    titulo: "1 Raquete Tibhar + 1 Bolinha Tibhar",
+    subtitulo: "R$ 295,00",
+    imagem: "figure_produto3",
+  },
+  {
+    id: 4,
+    titulo: "1 Rede Joola + Suporte Interno",
+    subtitulo: "R$ 45,00",
+    imagem: "figure_produto4",
+  },
 ];
+
+cardLista.forEach((card) => {
+  const cardHTML = `
+
+    <div class="div_produto">
+            <figure id="${card.imagem}">
+            </figure>
+            <h3>${card.titulo}</h3>
+            <h2>${card.subtitulo}</h2>
+            <button type="submit">Comprar</button>
+    </div>
+    `;
+
+  container.innerHTML += cardHTML;
+});
